318dab897a8b204157e70ace195c0479e6106ac6
Muhammed Shendi <muhammedshendi@Muhammeds-MacBook-Pro.local>
2021-05-06 12:01:32 +0200

    initial server setup

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+// setup express
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`Server started on port: ${PORT} - http://localhost:${PORT}`)
+);
+
+// setup mongoose
+mongoose.connect(
+  process.env.MONGODB_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (err) return console.error(err);
+    console.log("Mongodb connection established");
+  }
+);
